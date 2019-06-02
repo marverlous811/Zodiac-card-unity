43bfa3a0c5be82595517080fee1681d02fd74258
hieunq <hieunq@ows.vn>
2019-06-02 23:43:09 +0700

    create change scene

--- Assets/Script/LoginScreen.cs
+++ Assets/Script/LoginScreen.cs
@@ -2,37 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoginScreen : MonoBehaviour
 {
-    // public InputField nameField;
-    // public InputField roomField;
-    // public InputField serverEndPoint;
-    // public Button loginButton;
+    public InputField nameField;
+    public InputField roomField;
+    public InputField serverEndPoint;
+    public Button loginButton;
 
-    // private Rect windowRect = new Rect(0,0,Screen.width, Screen.height); 
+    private Rect windowRect = new Rect(0,0,Screen.width, Screen.height); 
 
-    // void Start()
-    // {
-    //     GUI.Window(0, windowRect, this.WindowFuntion, "login");
-    //     //Subscribe to onClick event
-    //     // loginButton.onClick.AddListener(adminDetails);
-    // }
+    void Start()
+    {
+        //Subscribe to onClick event
+        loginButton.onClick.AddListener(adminDetails);
+    }
 
-    // public void adminDetails()
-    // {
-    //     string name = nameField.text;
-    //     string room = roomField.text;
-    //     string server = serverEndPoint.text;
+    public void adminDetails()
+    {
+        string name = nameField.text;
+        string room = roomField.text;
+        string server = serverEndPoint.text;
 
-    //     Debug.Log("user: "+ name + " connect to " + server + " room " + room);
-    // }
+        SceneManager.LoadScene("GameScene");
 
-    // private void OnGUI() {
+        Debug.Log("user: "+ name + " connect to " + server + " room " + room);
+    }
+
+    private void OnGUI() {
         
-    // }
-
-    // void WindowFunction(int windowId){
-
-    // }
+    }
 }
